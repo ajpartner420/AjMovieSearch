8d2d05313e4c6d48fa0a02a478bec99e8c8eae32
Ariyanekyodha <113589454+Ariyanekyodha@users.noreply.github.com>
2024-01-01 04:52:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-if [ -z $UPSTREAM_REPO ]
+https://github.com/Ariyanekyodha/AjMovieSearch if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/lazyindu/Lazyrrr.git /LazyPrincess
+  git clone https://github.com/Ariyanekyodha/AjMovieSearch.git /AjMovieSearch
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /AjMovieSearch
 fi
-cd /LazyPrincess
+cd /AjMovieSearch
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
